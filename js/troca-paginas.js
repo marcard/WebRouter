c20f08652291a786fe66082b0e543cc55b1a28fe
marcard <marcard.mx@outlook.com>
2019-12-06 17:14:57 -0400

    FIrst COmmit

---
+++ js/troca-paginas.js
@@ -0,0 +1,26 @@
+function home_pg()
+{
+	
+	window.location.href = "home.html";
+
+	window.location.assign("home.html");
+
+}
+
+function sobre_pg()
+{
+
+	window.location.href = "sobre.html";
+
+	window.location.assign("sobre.html");
+
+}
+
+function servicos_pg()
+{
+
+	window.location.href = "servicos.html";
+
+	window.location.assign("servicos.html");
+
+}
